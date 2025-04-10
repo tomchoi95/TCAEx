7b1e85412d59138ebb6a1ec20e228b7e7d9d1a49
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-10 20:15:14 +0900

    Reducer 매크로에 관한 정보 추가

--- TCAEx/CounterFeature.swift
+++ TCAEx/CounterFeature.swift
@@ -7,7 +7,11 @@
 
 import ComposableArchitecture
 
+// 아래의 Reducer 매크로가 Reducer프로토콜을 채택 + etc의 역할
+// 매크로에() 를 하면 @Reducer(state: <#T##_SynthesizedConformance...##_SynthesizedConformance#>, action: <#T##_SynthesizedConformance...##_SynthesizedConformance#>) 이렇게 됨.
+// 여기서 state와 action에게 codable, decodable, encodable, equatable, hashable, sendable을 채택할 수 있게 함.
+
 @Reducer
 struct CounterFeature {
-    
+    //
 }
